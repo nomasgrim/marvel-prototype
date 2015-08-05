87f7ef817a2905e80ac29b3c458eea47653ca8e0
cbare1208 <cbarendsen@vml.com>
2015-08-04 20:38:46 -0500

    better full stack

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('appApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
