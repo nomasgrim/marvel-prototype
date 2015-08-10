3e845ae9dc3e9126056be0d436c577ea2c09a3fd
cbare1208 <cbarendsen@vml.com>
2015-08-10 13:43:29 -0500

    updated merge to app.js, no more conflicts

--- application/app/scripts/app.js
+++ application/app/scripts/app.js
@@ -1,26 +1,24 @@
-<<<<<<< HEAD
 /*jshint unused: vars */
-define(['angular', 'controllers/main', 'controllers/comics', 'services/characters', 'services/comics', 'services/insert-characters', 'controllers/insert-characters', 'controllers/events', 'controllers/stories', 'controllers/insert-comics', 'controllers/insert-stories', 'controllers/insert-events', 'controllers/scrub-character-powers', 'controllers/character', 'services/character', 'controllers/comic', 'services/comic']/*deps*/, 
-  function (angular, MainCtrl, ComicsCtrl, CharactersService, ComicsService, InsertCharactersFactory, InsertCharactersCtrl, EventsCtrl, StoriesCtrl, InsertComicsCtrl, InsertStoriesCtrl, InsertEventsCtrl, ScrubCharacterPowersCtrl, CharacterCtrl, CharacterFactory, ComicCtrl, ComicFactory)/*invoke*/ {
-=======
-
 define([
-    'angular',
-    'controllers/main',
+    'angular', 
+    'controllers/main', 
     'controllers/comics',
     'controllers/powers',
-    'services/characters',
-    'services/comics',
-    'services/insert-characters',
-    'controllers/insert-characters',
-    'controllers/events',
-    'controllers/stories',
-    'controllers/insert-comics',
-    'controllers/insert-stories',
-    'controllers/insert-events',
-    'controllers/scrub-character-powers']/*deps*/, 
-  function (angular, MainCtrl, ComicsCtrl, PowersCtrl, CharactersService, ComicsService, InsertCharactersFactory, InsertCharactersCtrl, EventsCtrl, StoriesCtrl, InsertComicsCtrl, InsertStoriesCtrl, InsertEventsCtrl, ScrubCharacterPowersCtrl)/*invoke*/ {
->>>>>>> 2a3cdaaeac5f870e2130c959d30ccd6257803c1c
+    'services/characters', 
+    'services/comics', 
+    'services/insert-characters', 
+    'controllers/insert-characters', 
+    'controllers/events', 
+    'controllers/stories', 
+    'controllers/insert-comics', 
+    'controllers/insert-stories', 
+    'controllers/insert-events', 
+    'controllers/scrub-character-powers', 
+    'controllers/character', 
+    'services/character', 
+    'controllers/comic', 
+    'services/comic']/*deps*/, 
+  function (angular, MainCtrl, ComicsCtrl, PowersCtrl, CharactersService, ComicsService, InsertCharactersFactory, InsertCharactersCtrl, EventsCtrl, StoriesCtrl, InsertComicsCtrl, InsertStoriesCtrl, InsertEventsCtrl, ScrubCharacterPowersCtrl, CharacterCtrl, CharacterFactory, ComicCtrl, ComicFactory)/*invoke*/ {
   'use strict';
 
   /**
@@ -32,24 +30,23 @@ define([
    * Main module of the application.
    */
   return angular
-<<<<<<< HEAD
     .module('appApp', ['appApp.controllers.MainCtrl',
-'appApp.controllers.ComicsCtrl',
-'appApp.services.Comic',
-'appApp.services.Comics',
-'appApp.services.Character',
-'appApp.services.Characters',
-'appApp.services.InsertCharacters',
-'appApp.controllers.InsertCharactersCtrl',
-'appApp.controllers.EventsCtrl',
-'appApp.controllers.StoriesCtrl',
-'appApp.controllers.InsertComicsCtrl',
-'appApp.controllers.InsertStoriesCtrl',
-'appApp.controllers.InsertEventsCtrl',
-'appApp.controllers.ScrubCharacterPowersCtrl',
-'appApp.controllers.CharacterCtrl',
-'appApp.controllers.ComicCtrl',
-/*angJSDeps*/
+    'appApp.controllers.ComicsCtrl',
+    'appApp.services.Comic',
+    'appApp.services.Comics',
+    'appApp.controllers.Powers',
+    'appApp.services.Character',
+    'appApp.services.Characters',
+    'appApp.services.InsertCharacters',
+    'appApp.controllers.InsertCharactersCtrl',
+    'appApp.controllers.EventsCtrl',
+    'appApp.controllers.StoriesCtrl',
+    'appApp.controllers.InsertComicsCtrl',
+    'appApp.controllers.InsertStoriesCtrl',
+    'appApp.controllers.InsertEventsCtrl',
+    'appApp.controllers.ScrubCharacterPowersCtrl',
+    'appApp.controllers.CharacterCtrl',
+    'appApp.controllers.ComicCtrl',
     'ngCookies',
     'ngResource',
     'ngSanitize',
@@ -94,6 +91,11 @@ define([
           controller: 'EventsCtrl',
           controllerAs: 'events'
         })
+        .when('/powers', {
+          templateUrl: 'views/comics.html',
+          controller: 'PowersCtrl',
+          controllerAs: 'powersCtrl'
+        })
         .when('/stories', {
           templateUrl: 'views/stories.html',
           controller: 'StoriesCtrl',
@@ -130,98 +132,4 @@ define([
           redirectTo: '/'
         });
     });
-=======
-    .module('appApp', [
-      'appApp.controllers.MainCtrl',
-      'appApp.controllers.ComicsCtrl',
-      'appApp.services.Comics',
-      'appApp.controllers.Powers',
-      'appApp.services.Characters',
-      'appApp.services.InsertCharacters',
-      'appApp.controllers.InsertCharactersCtrl',
-      'appApp.controllers.EventsCtrl',
-      'appApp.controllers.StoriesCtrl',
-      'appApp.controllers.InsertComicsCtrl',
-      'appApp.controllers.InsertStoriesCtrl',
-      'appApp.controllers.InsertEventsCtrl',
-      'appApp.controllers.ScrubCharacterPowersCtrl',
-      /*angJSDeps*/
-      'ngCookies',
-      'ngResource',
-      'ngSanitize',
-      'ngRoute',
-      'ngAnimate',
-      'ngTouch'
-    ])
-    .config(appConfig);
-
-  function appConfig ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/comics', {
-        templateUrl: 'views/comics.html',
-        controller: 'ComicsCtrl',
-        controllerAs: 'comicsCtrl'
-      })
-      .when('/comics/:offset', {
-        templateUrl: 'views/comics.html',
-        controller: 'ComicsCtrl',
-        controllerAs: 'comicsCtrl'
-      })
-      .when('/insert-characters', {
-        templateUrl: 'views/insert-characters.html',
-        controller: 'InsertCharactersCtrl',
-        controllerAs: 'insertCharactersCtrl'
-      })
-      .when('/events', {
-        templateUrl: 'views/events.html',
-        controller: 'EventsCtrl',
-        controllerAs: 'events'
-      })
-      .when('/powers', {
-        templateUrl: 'views/comics.html',
-        controller: 'PowersCtrl',
-        controllerAs: 'powersCtrl'
-      })
-      .when('/stories', {
-        templateUrl: 'views/stories.html',
-        controller: 'StoriesCtrl',
-        controllerAs: 'stories'
-      })
-      .when('/insert-comics', {
-        templateUrl: 'views/insert-comics.html',
-        controller: 'InsertComicsCtrl',
-        controllerAs: 'insertComics'
-      })
-      .when('/insert-stories', {
-        templateUrl: 'views/insert-stories.html',
-        controller: 'InsertStoriesCtrl',
-        controllerAs: 'insertStories'
-      })
-      .when('/insert-events', {
-        templateUrl: 'views/insert-events.html',
-        controller: 'InsertEventsCtrl',
-        controllerAs: 'insertEvents'
-      })
-      .when('/scrub-character-powers', {
-        templateUrl: 'views/scrub-character-powers.html',
-        controller: 'ScrubCharacterPowersCtrl',
-        controllerAs: 'scrubCharacterPowers'
-      })
-      // this needs to be the 2nd to last
-      // TODO: fix so it doesn't have to be the 2nd to last
-      .when('/:offset', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }
->>>>>>> 2a3cdaaeac5f870e2130c959d30ccd6257803c1c
 });
